03246e33153d97a40d9e6b1a8ce763af9ea450d7
Ömer Emeksiz <omeremksz@gmail.com>
2023-08-31 09:39:28 +0300

    Closes #1 - Both frontend and backend part of the project is created and run succesfully. Also backend connected to local database

---
+++ backend/studentforum/src/test/java/com/omerfurkan/studentforum/StudentforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omerfurkan.studentforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
